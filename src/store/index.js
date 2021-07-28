8d8c7ccaddb6d81539799e5a9e6d4448097bfa26
emir <emir05051@gmail.com>
2021-07-28 20:29:55 +0600

    maste

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,13 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: { todoList: [] },
+  mutations: {
+    ADD_TODO(state, todo) {
+      state.todoList.push(todo);
+    },
+    SET_TODOS(state, todo)
+  },
   actions: {},
   modules: {},
 });
